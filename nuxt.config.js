2e86e82601bcd8e7104e9771e0dd5634266c04dc
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-07-22 11:32:23 +0200

    app

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['nuxt-electron'],
+  electron: {
+    build: [
+      {
+        // Main-Process entry file of the Electron App.
+        entry: "electron/main.ts",
+      },
+      {
+        entry: "electron/preload.ts",
+        onstart(args) {
+          args.reload();
+        },
+      },
+    ],
+  },
+})
